fccdd37ba32a8afc93b9ec13fb47b4c391ae33fd
Tércio <fracelino2011@gmail.com>
2024-10-17 09:47:45 -0300

    No maven, problem with Tercio PC

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import VetorialOperacoes.Vetorial;
 import entities.Vetor;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         Vetorial vetorial = new Vetorial();
+
 
         Vetor v = new Vetor(1, 2, 3);
         Vetor w = new Vetor(4, 5, 6);
